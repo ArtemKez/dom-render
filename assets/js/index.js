512ba6a447b9a68307cfa6fbfee195d004fd8572
ArtemKez <artem.kez.work@gmail.com>
2021-11-20 17:55:00 +0200

    final commit

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,28 +1,37 @@
 'use strict';
 
 const messageArray = [];
-
+const lists = document.getElementById('lists');
 const form = document.getElementById('root-form');
 
-form.addEventListener('submit', (e)=>{
-  e.preventDefault();
-  const {target: { textInput }} = e;
-  //после нажатия на кнопку добавлять значение инпута в массив, если оно не пустое
-  const inputValue = textInput.value.trim();
-  if(inputValue){
-    messageArray.push(inputValue);
-    //после добавления - очищать форму
-    form.reset();
-  }
-  //рендерить на странице то, что добавилось в массив
-  console.dir(messageArray);
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const {target: {textInput}} = e;
+    //после нажатия на кнопку добавлять значение инпута в массив, если оно не пустое
+    const inputValue = textInput.value.trim();
+    if (inputValue) {
+        messageArray.push(inputValue);
+        form.reset();
+    }
+    renderList();
+    console.dir(messageArray);
 })
 
+function renderList() {
+    lists.innerHTML = null;
+    for (const value of messageArray) {
+        lists.append(createElement('li', {}, value));
+    }
+}
 
-function createElement(type,{classNames, typeEvent, onClick}, ...children){
-  const elem = document.createElement(type);
-  elem.classList.add(...classNames);
-  elem.addEventListener(typeEvent, onClick);//for example
-  elem.append(...children);
-  return elem;
+
+function createElement(type, {
+    classNames = [], typeEvent = {}, onClick = () => {
+    }
+}, ...children) {
+    const elem = document.createElement(type);
+    elem.classList.add(...classNames);
+    elem.addEventListener(typeEvent, onClick);//for example
+    elem.append(...children);
+    return elem;
 }
